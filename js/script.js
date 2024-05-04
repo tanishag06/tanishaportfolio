6fb3e6e7f238da7c91113769e223d51bcde435e3
tanishag06 <167208762+tanishag06@users.noreply.github.com>
2024-05-04 10:53:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".typing",{
-    strings:["UI Designer","Web Developer","Graphic Designer","UX Researcher"],
+    strings:["UI Designer","UX Researcher","Graphic Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
